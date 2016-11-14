817b957cc2b9cd33b2fca789d5d107c8502b165c
Jun Woo Shin <shin.junwoo97@gmail.com>
2016-11-13 21:14:15 -0500

    get visible inputs

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,1 +1,6 @@
 'use strict';
+
+const utils = window.formsafe.utils;
+const contentUtils = window.formsafe.contentUtils;
+
+const visibleInputs = Array.from(document.querySelectorAll('input')).filter((elem) => utils.isVisible(elem));
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -2,4 +2,30 @@
 
 window.formsafe = window.formsafe || {};
 
-window.formsafe.utils = {};
+function isVisible(elem) {
+	if (!elem || !elem.nodeName) {
+		return false;
+	}
+
+	const boundingRect = elem.getBoundingClientRect();
+	const keys = [
+		'bottom',
+		'height',
+		'left',
+		'right',
+		'top',
+		'width'
+	];
+
+	for (let prop of keys) {
+		if (boundingRect[prop]) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+window.formsafe.utils = {
+	isVisible: isVisible
+};
